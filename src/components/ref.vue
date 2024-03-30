3e57da740bf3e2247bcb72ac5d061977221c1ad2
李思源 <2574561698@qq.com>
2024-03-30 09:16:20 +0800

    使用elementplus 使用ref双向绑定

---
+++ src/components/ref.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ name }}</div>
+  <el-button :onclick="changeName" type="primary">改变姓名</el-button>
+  <div>{{ age }}</div>
+  <el-button :onclick="changeAge" type="primary">增加</el-button>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+const name = ref('张三')
+const age = ref(1)
+//改变name.value进行双向数据绑定
+const changeName = () => {
+  name.value = '李四'
+}
+const changeAge = () => {
+  age.value += 1
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/day1.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- 使用ref进行双向数据绑定 -->
+  <RefComponents />
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import RefComponents from '@/components/ref.vue'
+</script>
+
+<style scoped></style>
